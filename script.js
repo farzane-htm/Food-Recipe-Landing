b24b3ef3048836477b6a826b33c8fe3d3a876a2c
farzane-htm <farzane.htmi@gmail.com>
2025-08-16 23:12:08 +0330

    init project

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+const searchBox = document.querySelector(".searchBox");
+const searchBtn = document.querySelector(".searchBtn");
+const recipeContainer = document.querySelector(".recipe-container");
+
+searchBtn.addEventListener("click", () => {
+  console.log("Button Clicked");
+});
